239badc45285e665785697585db92e990c087e81
MosheM123 <158142047+MosheM123@users.noreply.github.com>
2024-02-27 08:58:30 +0200

    Update settings.py

--- myweb/settings.py
+++ myweb/settings.py
@@ -82,14 +82,7 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#database
 
-DATABASES = {
-    'default':{
-        'ENGINE':'django.db.backends.postgresql',
-        'NAME': 'test3',
-        'USER': 'moni3',
-        'PASSWORD': 'Mertelya2',
-    }
-}
+
 
 
 # Password validation
